3d09db2154a4829fce0f3a207d89d2bce723ab68
Abdurahmon <abdutuit@gmail.com>
2023-02-21 16:08:42 +0500

    first game

---
+++ stylehelp.py
@@ -0,0 +1,16 @@
+class StyleSheet:
+    @staticmethod
+    def activate():
+        
+        return """
+        background-color:rgb(34, 255, 108);
+        font-size:60px;
+        color:rgb(0,0,0);
+        """
+    @staticmethod
+    def not_active():
+        return """
+        background-color:rgb(255, 85, 29);
+        font-size:30px;
+        color:rgb(0,0,0);
+        """
